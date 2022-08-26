c3aaf26ff8ce53e5ebbed90dd1b295fb84b4b68c
Ernest Cui <ecuiships@126.com>
2022-08-26 11:03:12 +0800

    added keyboard control support

--- logics/index.ts
+++ logics/index.ts
@@ -1,6 +1,9 @@
 import Decimal from "decimal.js";
 import calculatorMemory from "../observables/calculator-memory";
 import stringsRes from "../observables/strings-res";
+import * as LS from "./sys-keys";
+import * as LF from "./func-keys";
+import * as LB from "./basic-keys";
 
 export const initialize = () => {
     Decimal.set({
@@ -9,4 +12,92 @@ export const initialize = () => {
 
     calculatorMemory.loadFromLocalStorage();
     stringsRes.switchLangFromStorage();
+
+    window.onkeydown = onWindowKeydown;
+}
+
+const onWindowKeydown = (e: KeyboardEvent) => {
+    switch (e.key) {
+        case "0":
+            LB.onR4C1Click();
+            break;
+        case ".":
+            LB.onR4C2Click();
+            break;
+        case "Enter":
+            LB.onR4C5Click();
+            break;
+        case "1":
+            LB.onR3C1Click();
+            break;
+        case "2":
+            LB.onR3C2Click();
+            break;
+        case "3":
+            LB.onR3C3Click();
+            break;
+        case "+":
+            LB.onR3C4Click();
+            break;
+        case "-":
+            LB.onR3C5Click();
+            break;
+        case "4":
+            LB.onR2C1Click();
+            break;
+        case "5":
+            LB.onR2C2Click();
+            break;
+        case "6":
+            LB.onR2C3Click();
+            break;
+        case "*":
+            LB.onR2C4Click();
+            break;
+        case "/":
+            if (e.code === "NumpadDivide") {
+                LB.onR2C5Click();
+            }
+            else {
+                LF.onR2C1Click();
+            }
+            
+            break;
+        case "7":
+            LB.onR1C1Click();
+            break;
+        case "8":
+            LB.onR1C2Click();
+            break;
+        case "9":
+            LB.onR1C3Click();
+            break;
+        case "Backspace":
+            LB.onR1C4Click();
+            break;
+        case "Delete":
+            LB.onR1C5Click();
+            break;
+        case "Shift":
+            LS.onShiftClick();
+            break;
+        case "ArrowUp":
+            LS.onDirClick("U");
+            break;
+        case "ArrowDown":
+            LS.onDirClick("D");
+            break;
+        case "ArrowLeft":
+            LS.onDirClick("L");
+            break;
+        case "ArrowRight":
+            LS.onDirClick("R");
+            break;
+        case "'":
+            LF.onR3C2Click();
+            break;
+        case ",":
+            LF.onR4C5Click();
+            break;
+    }
 }
